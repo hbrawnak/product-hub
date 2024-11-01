e9171599348ddd8acd7fbc72d429fffe8ecd7ce8
Habibur Rahman <hbrawnak@gmail.com>
2024-11-02 03:26:40 +0600

    added product routes prefix

--- routes.go
+++ routes.go
@@ -2,6 +2,9 @@ package main
 
 func (app *App) handleRoutes() {
 	app.Router.HandleFunc("/", app.GetHome).Methods("GET").Name("home")
-	app.Router.HandleFunc("/products", app.GetProductList).Methods("GET").Name("product-list")
-	app.Router.HandleFunc("/products/{id}", app.GetProductById).Methods("GET").Name("product-details")
+
+	//Product API
+	products := app.Router.PathPrefix("/api/products").Subrouter()
+	products.HandleFunc("/", app.GetProductList).Methods("GET").Name("product-list")
+	products.HandleFunc("/{id}", app.GetProductById).Methods("GET").Name("product-details")
 }
