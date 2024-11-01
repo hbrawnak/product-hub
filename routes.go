29843874657e9b93a14c1fe954d00161a11e44d0
Habibur Rahman <hbrawnak@gmail.com>
2024-11-02 03:17:58 +0600

    fixed routes name

--- routes.go
+++ routes.go
@@ -2,6 +2,6 @@ package main
 
 func (app *App) handleRoutes() {
 	app.Router.HandleFunc("/", app.GetHome).Methods("GET").Name("home")
-	app.Router.HandleFunc("/products", app.GetProductList).Methods("GET").Name("home")
-	app.Router.HandleFunc("/products/{id}", app.GetProductById).Methods("GET").Name("home")
+	app.Router.HandleFunc("/products", app.GetProductList).Methods("GET").Name("product-list")
+	app.Router.HandleFunc("/products/{id}", app.GetProductById).Methods("GET").Name("product-details")
 }
